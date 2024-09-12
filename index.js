57181a8d820e9d42bcb8e11e72a51bf99a52fa3a
Anderson Brasil <andersonbrasil72@gmail.com>
2024-09-12 16:09:49 -0300

    Push

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import { getMessaging } from "firebase-admin/messaging";
 import express, { json } from "express";
 import cors from "cors";
 
+import { createRequire } from 'node:module'
+
+// Import the package.json file to get the version number by using the createRequire function
+const require = createRequire(import.meta.url)
+const serviceAccount = require('./applojasolar-firebase-adminsdk-8a493-34a60ce174.json')
+
 // const credAccess = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-import serviceAccount from "./applojasolar-firebase-adminsdk-8a493-34a60ce174.json" assert { type: "json" };
+//import serviceAccount from "./applojasolar-firebase-adminsdk-8a493-34a60ce174.json" assert { type: "json" };
 const app = express();
 app.use(express.json());
 
@@ -44,13 +50,13 @@ app.post("/send", function (req, res) {
     //   image: "https://lojasolar.vtexassets.com/arquivos/ids/177192-1200-auto"
     // },
 
-      token: "eKCBtqjoRsy57tgvK1b6Pn:APA91bHEJL9CvaDrPf6RCzNUKKPD17KeiAsoqDpfRcNJLgdYr0OVi5XMQojNYsgI5kQrPfwgZOQpc4mnV0q3kTMgaQ9-3zoAEpW44_XDyekvvhSu4CApFJ_E_URD4AoZS3bpXE5o8Y99",
-      notification: {
-        title: "Bom dia, bem vindo.",
-        body: "Seja bem vindo as notificações do novo app solar!",
-        // url: "https://www.lojasolar.com.br/carrinho-de-mao-cacamba-50l-azul-77704-432-tramontina/p",
-        image: "https://lojasolar.vtexassets.com/arquivos/ids/177192-1200-auto"
-      }
+    token: "eeHjZ9rKQgG-eP8jw-Y28Z:APA91bFHiypw_-CIpssDGcXOpJanL47iEYcEAyf_Ob8C44PBt8d7KhnupAykR_hpw0CVAvSpq51vCHg5Orzsudj_EAPempp2WFkq4UgXUJKfjjcd27t1616ILSSrgLL6cix4O19qP3Ep",
+    data: {
+      title: "Bom dia, bem vindo.",
+      body: "Seja bem vindo as notificações do novo app solar!",
+      url: "https://www.lojasolar.com.br/carrinho-de-mao-cacamba-50l-azul-77704-432-tramontina/p",
+      image: "https://lojasolar.vtexassets.com/arquivos/ids/177192-1200-auto"
+    }
   };
 
   getMessaging()
